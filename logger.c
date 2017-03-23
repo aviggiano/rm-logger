135b518703cc42e4d413b7122996aeb0585c89e8
Antonio Viggiano <agfviggiano@gmail.com>
2017-03-23 09:31:26 -0300

    better comments

--- logger.c
+++ logger.c
@@ -77,12 +77,12 @@ int LoggerHistoryCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc
 }
 int RedisModule_OnLoad(RedisModuleCtx *ctx) {
 
-    // Register the module itself – it’s called example and has an API version of 1
+    // Register the module itself 
     if (RedisModule_Init(ctx, "LOGGER", 1, REDISMODULE_APIVER_1) == REDISMODULE_ERR) {
         return REDISMODULE_ERR;
     }
 
-    // register our command – it is a write command, with one key at argv[1]
+    // register our command
     if (RedisModule_CreateCommand(ctx, "LOGGER.PUB", LoggerPubCommand, "write pubsub", 1, 1, 1) == REDISMODULE_ERR) {
         return REDISMODULE_ERR;
     }
